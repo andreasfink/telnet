5267cc0f7c02067f43392f23eb41cc9dff8d5494
Andreas Fink <andreas@fink.org>
2017-07-19 00:34:17 +0200

    Initial Commit

---
+++ telnet/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  telnet
+//
+//  Created by Andreas Fink on 19.07.17.
+//  Copyright © 2017 Andreas Fink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
